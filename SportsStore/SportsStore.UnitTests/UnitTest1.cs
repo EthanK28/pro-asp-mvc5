52b242abf17fb696a00833fa4bdc585f0e5b8386
EthanK28 <kes0228@gmail.com>
2015-12-03 08:05:00 +0900

    사이드 메뉴  생성

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -33,7 +33,7 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             // Act 
-            ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel)controller.List(null, 2).Model;
             
 
             // Assert
@@ -89,7 +89,7 @@ namespace SportsStore.UnitTests
 
 
             // Act
-            ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel)controller.List(null, 2).Model;
 
             // Assert
             PagingInfo pageInfo = result.PagingInfo;
@@ -98,5 +98,84 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(pageInfo.TotalItems, 5);
             Assert.AreEqual(pageInfo.TotalPages, 2);
         }
+
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            // Arrange
+            // - Mock 리파지토리를 생성한다. 
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category="Cat1" },
+                new Product { ProductID = 2, Name = "P2", Category="Cat2" },
+                new Product { ProductID = 3, Name = "P3", Category="Cat1" },
+                new Product { ProductID = 4, Name = "P4", Category="Cat2" },
+                new Product { ProductID = 5, Name = "P5", Category="Cat3" }
+            });
+
+            // Arrange
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Action
+            Product[] result = ((ProductListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+
+            // Assert 
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange - Mock 리파지토리 생성
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category="Apples" },
+                new Product { ProductID = 2, Name = "P2", Category="Apples" },
+                new Product { ProductID = 3, Name = "P3", Category="Plums" },
+                new Product { ProductID = 4, Name = "P4", Category="Oranges" }
+            });
+
+            // Arragne - 컨트롤러를 생성
+            NavController target = new NavController(mock.Object);
+
+            // Act
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category="Apples" },
+                new Product { ProductID = 4, Name = "P2", Category="Oranges" }
+            });
+
+            // Arrange - 컨트롤러 생성
+            NavController target = new NavController(mock.Object);
+            // Arrange - 선택될 카테고리 지정
+            string categoryToSelect = "Apples";
+
+            // Action
+            string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+            // Assert
+            Assert.AreEqual(categoryToSelect, result);
+
+        }
+        
     }
 }
